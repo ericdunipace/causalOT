72ec782b17ca625e90570c4396685e78d3971a42
ericdunipace <edunipace@g.harvard.edu>
2020-01-30 18:21:06 -0500

    combine simulation function

---
+++ combine_sims.R
@@ -0,0 +1,27 @@
+library(causalOT)
+
+#### Output settings ####
+jobids      <- as.character(c(41809323, 41856419, 41854105))
+n           <- "512"
+p           <- "6"
+design      <- "B"
+overlap     <- "low"
+simsett     <- "hainmueller"
+curdate     <- as.character(Sys.Date())
+
+#### Get Data ####
+path        <- file.path("Output", design, overlap, n, p)
+files       <- list.files(path = path)
+sel.files   <- unlist(c(sapply(jobids, grep, files, value = TRUE)))
+file.holder <- vector("list", length(sel.files))
+for(i in seq_along (sel.files) ) {
+  file.holder[[i]] <- readRDS(file.path(path, sel.files[i]))
+}
+
+#### Combine ####
+output      <- causalOT:::combine_sims(file.holder, simulation_name = simsett)
+
+outname     <- paste(c(simsett, "des", design, "overlap", overlap, "n", n, "date", curdate), collapse="_")
+saveRDS(output, file = file.path("Output", paste0(outname,".rds")))
+
+
