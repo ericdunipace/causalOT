a5a03eed957a9d647945f0ff4ed69dd9212f8214
ericdunipace <edunipace@g.harvard.edu>
2020-01-29 20:53:03 -0500

    class setting

--- R/ESS.R
+++ R/ESS.R
@@ -6,6 +6,7 @@ ESS.causalWeights <- function(x) {
   return(c("Control" = ESS(x$w0), "Treated" = ESS(x$w1)))
 }
 
+setOldClass("causalWeights")
 setGeneric("ESS", function(x) UseMethod("ESS"))
 setMethod("ESS", signature(x = "numeric"), ESS.default)
 setMethod("ESS", signature(x = "causalWeights"), ESS.causalWeights)
